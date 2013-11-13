ccb154216ba2fadf897197ab290c8ecacd10ef74
michele.mostarda <michele.mostarda@6c19a1f1-6440-3f7d-bc9a-635414ec97c4>
2013-11-13 15:54:28 +0000

    Added virtuoso integration for sesame drivers.

---
+++ virtuoso-sesame-triplestore/src/main/java/org/sindice/rdfcommons/storage/virtuoso/sesame/VirtuosoSparqlEndPoint.java
@@ -0,0 +1,164 @@
+/*
+ * Copyright 2008-2010 Digital Enterprise Research Institute (DERI)
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.sindice.rdfcommons.storage.virtuoso.sesame;
+
+import org.openrdf.query.BooleanQuery;
+import org.openrdf.query.GraphQuery;
+import org.openrdf.query.GraphQueryResult;
+import org.openrdf.query.MalformedQueryException;
+import org.openrdf.query.QueryEvaluationException;
+import org.openrdf.query.QueryLanguage;
+import org.openrdf.query.TupleQuery;
+import org.openrdf.query.TupleQueryResult;
+import org.openrdf.query.parser.ParsedQuery;
+import org.openrdf.repository.RepositoryException;
+import org.sindice.rdfcommons.adapter.sesame.SesameConversionUtil;
+import org.sindice.rdfcommons.model.TripleBuffer;
+import org.sindice.rdfcommons.model.TripleSet;
+import org.sindice.rdfcommons.storage.ResultSet;
+import org.sindice.rdfcommons.storage.SparqlEndPoint;
+import org.sindice.rdfcommons.storage.SparqlEndpointException;
+import virtuoso.sesame2.driver.VirtuosoRepositoryConnection;
+
+/**
+ * <i>Virtuoso</i> implementation of {@link org.sindice.rdfcommons.storage.SparqlEndPoint}.
+ *
+ * @author Michele Mostarda ( michele.mostarda@gmail.com )
+ * @version $Id$
+ */
+public class VirtuosoSparqlEndPoint implements SparqlEndPoint {
+
+    protected static final SesameConversionUtil SESAME_CONVERSION_UTIL = SesameConversionUtil.getInstance();
+
+    private final VirtuosoRepositoryConnection repositoryConnection;
+
+    protected VirtuosoSparqlEndPoint(VirtuosoRepositoryConnection repositoryConnection) {
+        this.repositoryConnection = repositoryConnection;
+    }
+
+    public ResultSet processSelectQuery(String qry)
+    throws SparqlEndpointException {
+        final TupleQuery tupleQuery;
+        try {
+            tupleQuery = repositoryConnection.prepareTupleQuery(QueryLanguage.SPARQL, qry);
+        } catch (MalformedQueryException mqe) {
+            throw new SparqlEndpointException("Error while parsing query", mqe);
+        } catch (RepositoryException re) {
+            throw new SparqlEndpointException("Error while connecting with repository.", re);
+        }
+
+        final TupleQueryResult tupleQueryResult;
+        try {
+            tupleQueryResult = tupleQuery.evaluate();
+        } catch (QueryEvaluationException qee) {
+            throw new SparqlEndpointException("Error while evaluating query.", qee);
+        }
+        return new VirtuosoResultSet(tupleQueryResult);
+    }
+
+    public TripleSet processConstructQuery(String qry) throws SparqlEndpointException {
+        return processGraphQuery(qry);
+    }
+
+    public TripleSet processDescribeQuery(String qry) throws SparqlEndpointException {
+        return processGraphQuery(qry);
+    }
+
+    public boolean processAskQuery(String qry) throws SparqlEndpointException {
+        final BooleanQuery booleanQuery;
+        try {
+            booleanQuery = repositoryConnection.prepareBooleanQuery(QueryLanguage.SPARQL, qry);
+        } catch (Exception e) {
+            throw new SparqlEndpointException("Error while parsing query.", e);
+        }
+        try {
+            return booleanQuery.evaluate();
+        } catch (QueryEvaluationException qee) {
+            throw new SparqlEndpointException("Error while evaluating query.", qee);
+        }
+    }
+
+    public EndpointResponse processQuery(String qry) throws SparqlEndpointException {
+        final ParsedQuery query;
+        try {
+            query = parseQuery(qry);
+        } catch (MalformedQueryException mqe) {
+            throw new SparqlEndpointException("Error while parsing query.", mqe);
+        }
+        switch ( getQueryType(query) ) {
+            case select:
+                return new EndpointResponse( QueryType.select, processSelectQuery(qry));
+            case construct:
+                return new EndpointResponse( QueryType.construct, processConstructQuery(qry));
+            case describe:
+                return new EndpointResponse( QueryType.describe, processDescribeQuery(qry));
+            case ask:
+                return new EndpointResponse( QueryType.ask, processAskQuery(qry));
+            default:
+                throw new SparqlEndpointException( String.format("Illegal query type for query[%s]", qry) );
+        }
+    }
+
+    public QueryType getQueryType(String qry) throws SparqlEndpointException {
+        try {
+            return  getQueryType( parseQuery(qry) );
+        } catch (MalformedQueryException mqe) {
+            throw new UnsupportedOperationException();
+        }
+    }
+
+    public QueryType getQueryType(ParsedQuery qry) throws SparqlEndpointException {
+        return null; // TODO: TBI
+    }
+
+    private ParsedQuery parseQuery(String qry) throws MalformedQueryException {
+//        final SPARQLParser sparqlParser =
+//                (SPARQLParser) QueryParserRegistry.getInstance().get(QueryLanguage.SPARQL).getParser();
+//        return sparqlParser.parseQuery(qry, null);
+        return null;
+    }
+
+    private TripleSet processGraphQuery(String qry) throws SparqlEndpointException {
+        final GraphQuery graphQuery;
+        try {
+            graphQuery = repositoryConnection.prepareGraphQuery(QueryLanguage.SPARQL, qry);
+        } catch (Exception e) {
+            throw new SparqlEndpointException("Error while parsing query.", e);
+        }
+
+        final GraphQueryResult queryResult;
+        try {
+            queryResult = graphQuery.evaluate();
+        } catch (QueryEvaluationException qee) {
+            throw new SparqlEndpointException("Error while evaluating query.", qee);
+        }
+
+        final TripleSet result = new TripleBuffer();
+        try {
+            while (queryResult.hasNext()) {
+                result.addTriple(
+                        SESAME_CONVERSION_UTIL.convertSesameStatementToTriple(queryResult.next())
+                );
+            }
+        } catch (Exception e) {
+            throw new SparqlEndpointException("Error while iterating query result.", e);
+        }
+        return result;
+    }
+
+}
+
---
+++ virtuoso-sesame-triplestore/src/test/java/org/sindice/rdfcommons/storage/virtuoso/sesame/VirtuosoDriverTestCase.java
@@ -0,0 +1,225 @@
+/*
+ * Copyright 2008-2010 Digital Enterprise Research Institute (DERI)
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.sindice.rdfcommons.storage.virtuoso.sesame;
+
+import org.openrdf.model.Graph;
+import org.openrdf.model.Literal;
+import org.openrdf.model.Resource;
+import org.openrdf.model.Statement;
+import org.openrdf.model.URI;
+import org.openrdf.model.Value;
+import org.openrdf.model.impl.GraphImpl;
+import org.openrdf.query.BindingSet;
+import org.openrdf.query.GraphQuery;
+import org.openrdf.query.MalformedQueryException;
+import org.openrdf.query.QueryEvaluationException;
+import org.openrdf.query.QueryLanguage;
+import org.openrdf.query.TupleQuery;
+import org.openrdf.query.TupleQueryResult;
+import org.openrdf.repository.RepositoryConnection;
+import org.openrdf.repository.RepositoryException;
+import org.openrdf.repository.RepositoryResult;
+import org.sindice.rdfcommons.adapter.sesame.RDFHelper;
+import org.testng.annotations.Test;
+import virtuoso.sesame2.driver.VirtuosoRepository;
+
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Tests the basic access to the <i>Virtuoso</i> triple storage.
+ *
+ * @author Michele Mostarda ( michele.mostarda@gmail.com )
+ * @version $Id: VirtuosoDriverTestCase.java 143 2011-05-23 21:41:46Z michele.mostarda $
+ */
+public class VirtuosoDriverTestCase {
+
+    private static final String NAMED_GRAPH_URI = "http://test/named/graph/sesame";
+
+    /**
+     * Test querying by triple match.
+     *
+     * @throws org.openrdf.repository.RepositoryException
+     */
+    @Test
+    public void testTriples() throws RepositoryException {
+        VirtuosoRepository repository = getTestRepository();
+        RepositoryConnection connection = repository.getConnection();
+        addTestTriples(connection);
+        RepositoryResult result = connection.getStatements(null, null, null, true, (Resource) null);
+        List<Statement> statements = toList(result);
+        assert statements.size() > 0 : "Expected something.";
+        connection.close();
+    }
+
+
+    /**
+     * Test querying by SPARQL endpoint.
+     *
+     * @throws java.sql.SQLException
+     * @throws org.openrdf.query.MalformedQueryException
+     * @throws org.openrdf.query.QueryEvaluationException
+     * @throws org.openrdf.repository.RepositoryException
+     */
+    @Test
+    public void testDataRetrieval()
+    throws SQLException, RepositoryException, MalformedQueryException, QueryEvaluationException {
+        VirtuosoRepository repository = getTestRepository();
+        RepositoryConnection connection = repository.getConnection();
+        try {
+        TupleQuery query = connection.prepareTupleQuery(
+                QueryLanguage.SPARQL,
+                "SELECT * WHERE { GRAPH ?graph { ?s ?p ?o } } limit 100"
+        );
+		TupleQueryResult results = query.evaluate();
+		int counter = 0;
+        while (results.hasNext()) {
+			BindingSet bindingSet = results.next();
+		    Value graph = bindingSet.getValue("graph");
+		    Value s = bindingSet.getValue("s");
+		    Value p = bindingSet.getValue("p");
+		    Value o = bindingSet.getValue("o");
+            assert graph != null && s != null && p != null && o != null;
+            counter++;
+        }
+
+        assert counter == 100 : "Unexpected graph size: " + counter ;
+        } finally {
+            connection.close();
+        }
+    }
+
+    /**
+     * Tests the literal support.
+     *
+     * @throws org.openrdf.query.MalformedQueryException
+     * @throws org.openrdf.query.QueryEvaluationException
+     * @throws org.openrdf.repository.RepositoryException
+     */
+    @Test
+    public void testLiteralSupport() throws RepositoryException, MalformedQueryException, QueryEvaluationException {
+        final int SIZE = 100;
+
+        Graph graph = new GraphImpl();
+        for(int i = 0; i < SIZE; i++) {
+            URI sub = RDFHelper.uri("http://test#Sub" + i);
+            URI pre = RDFHelper.uri("http://test#pre");
+            Literal obj = RDFHelper.literal(123.456);
+            graph.add(sub, pre, obj);
+        }
+
+        VirtuosoRepository repository = getTestRepository();
+        RepositoryConnection connection = repository.getConnection();
+        connection.clear();
+        connection.add(graph);
+
+        assert executeQueryAndCountResults(
+                connection,
+                "PREFIX xsd: <http://www.w3.org/2001/XMLSchema#> SELECT * WHERE {?s <http://test#pre> 123.456 }"
+        ) == SIZE;
+        assert executeQueryAndCountResults(
+                connection,
+                "PREFIX xsd: <http://www.w3.org/2001/XMLSchema#> SELECT * WHERE " +
+                "{?s <http://test#pre> \"123.456\"^^xsd:double }"
+        ) == SIZE;
+        assert executeQueryAndCountResults(
+                connection,
+                "PREFIX xsd: <http://www.w3.org/2001/XMLSchema#> SELECT * WHERE {?s <http://test#pre> \"123.456\" }"
+        ) == 0;
+    }
+
+    /**
+     * Tests the ability of the driver to perform CONSTRUCT queries.
+     *
+     * @throws org.openrdf.query.MalformedQueryException
+     * @throws org.openrdf.query.QueryEvaluationException
+     * @throws org.openrdf.repository.RepositoryException
+     */
+    @Test
+    public void testConstruct() throws RepositoryException, MalformedQueryException, QueryEvaluationException {
+        final String qry = "PREFIX xsd: <http://www.w3.org/2001/XMLSchema#> " +
+                           "CONSTRUCT {?s ?p \"123.456\"^^xsd:double} " +
+                            String.format("FROM <%s> ", NAMED_GRAPH_URI) +
+                            "WHERE {?s ?p \"123.456\"^^xsd:double   }";
+        VirtuosoRepository repository = getTestRepository();
+        RepositoryConnection connection = repository.getConnection();
+        GraphQuery query = connection.prepareGraphQuery(QueryLanguage.SPARQL, qry);
+        query.evaluate();
+    }
+
+    /**
+     * Executes a query on the given graph.
+     *
+     * @param connection
+     * @param qry
+     * @return the number of retrieved triples.
+     * @throws org.openrdf.query.MalformedQueryException
+     * @throws org.openrdf.query.QueryEvaluationException
+     * @throws org.openrdf.repository.RepositoryException
+     */
+    private int executeQueryAndCountResults(RepositoryConnection connection, String qry)
+    throws QueryEvaluationException, RepositoryException, MalformedQueryException {
+        TupleQuery query = connection.prepareTupleQuery(QueryLanguage.SPARQL, qry);
+        TupleQueryResult trs = query.evaluate();
+
+        int count = 0;
+        while(trs.hasNext()) {
+            trs.next();
+            count++;
+        }
+        return count;
+    }
+
+    /**
+     * @return a named instance of the virtuoso graph.
+     */
+    private VirtuosoRepository getTestRepository() {
+		final String url = "jdbc:virtuoso://localhost:1111";
+        return new VirtuosoRepository(url, "dba", "dba", NAMED_GRAPH_URI);
+    }
+
+    /**
+     * @return an anonymous instance of the virtuoso graph.
+     */
+    private VirtuosoRepository createGraph() {
+		final String url = "jdbc:virtuoso://localhost:1111";
+        return new VirtuosoRepository(url, "dba", "dba");
+    }
+
+    private void addTestTriples(RepositoryConnection connection) throws RepositoryException {
+        connection.add(
+                RDFHelper.triple(
+                        RDFHelper.uri("http://fake/sub"),
+                        RDFHelper.uri("http://fake/pred"),
+                        RDFHelper.literal(
+                                "123.456",
+                                RDFHelper.uri("http://www.w3.org/2001/XMLSchema#double")
+                        )
+                )
+        );
+    }
+
+    private List<Statement> toList(RepositoryResult repositoryResult) throws RepositoryException {
+        final List<Statement> result = new ArrayList<Statement>();
+        while(repositoryResult.hasNext()) {
+            result.add((Statement) repositoryResult.next());
+        }
+        return result;
+    }
+
+}
